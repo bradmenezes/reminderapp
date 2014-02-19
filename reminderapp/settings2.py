75e71db70dec44b19bf3a1a0be4adad2cba4cbe9
Brad Menezes <bradmenezes10@gmail.com>
2014-02-18 21:31:31 -0800

    make config for prod

--- reminderapp/settings2.py
+++ reminderapp/settings2.py
@@ -9,17 +9,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'newdb',                      # Or path to database file if using sqlite3.
-#        # The following settings are not used with sqlite3:
-#        'USER': 'postgres',
-#        'PASSWORD': '',
-#        'HOST': '/tmp/',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#        'PORT': '5432',                      # Set to empty string for default.
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '/tmp/',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
+    }
+}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -154,7 +154,3 @@ LOGGING = {
         },
     }
 }
-
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
