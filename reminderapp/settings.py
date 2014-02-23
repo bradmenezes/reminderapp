647e8b9fc691834db2f58f06037abc93b55d05ae
Brad Menezes <bradmenezes10@gmail.com>
2014-02-23 14:14:31 -0800

    made base_settings.py and imported for local and prod

--- reminderapp/settings.py
+++ reminderapp/settings.py
@@ -1,18 +1,11 @@
 # Django settings for the project.
 
 try:
-    from settings_local import *
-except ImportError:
+    from base_settings import *
+except ImportError: 
     pass
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-
-MANAGERS = ADMINS
+DEBUG = False
 
 # heroku db settings
 import dj_database_url
