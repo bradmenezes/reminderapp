5a5696ff3e6c9bda835ceea0a417082dea7e5cc8
Brad Menezes <bradmenezes10@gmail.com>
2014-02-23 16:32:30 -0800

    added pywapi

--- reminderapp/base_settings.py
+++ reminderapp/base_settings.py
@@ -92,12 +92,7 @@ ROOT_URLCONF = 'reminderapp.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'reminderapp.wsgi.application'
 
-TEMPLATE_DIRS = (
-    '/Users/administrator/Dropbox/djangobrad/reminderapp/templates',
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -109,6 +104,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'reminderapp',
+    'pywapi',
 )
 
 # A sample logging configuration. The only tangible logging
