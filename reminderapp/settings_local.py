ae2ff3c3ad484b4d7bfbc911fa15d6177c3c23b0
Brad Menezes <bradmenezes10@gmail.com>
2014-02-23 16:12:15 -0800

    moved template dirs into local_settings

--- reminderapp/settings_local.py
+++ reminderapp/settings_local.py
@@ -21,3 +21,9 @@ DATABASES = {
     }
 }
 
+TEMPLATE_DIRS = (
+    '/Users/administrator/Dropbox/djangobrad/reminderapp/templates',
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
