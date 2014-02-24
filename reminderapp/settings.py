bb9db1549e31358860d419ac1a977b8690e84033
Brad Menezes <bradmenezes10@gmail.com>
2014-02-23 16:04:11 -0800

    templates dirs changes

--- reminderapp/settings.py
+++ reminderapp/settings.py
@@ -15,7 +15,8 @@ DATABASES = {
 }
 
 import os.path
-PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+#PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+PROJECT_DIR = here("..")
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, "templates"),
     # here you can add another templates directory if you wish.
