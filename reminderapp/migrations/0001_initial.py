7e1a8af4eca16c513f4c65593d1a52c0bfe667e9
bradmenezes <bradmenezes10@gmail.com>
2014-05-05 00:33:54 -0700

    changes to 0001

--- reminderapp/migrations/0001_initial.py
+++ reminderapp/migrations/0001_initial.py
@@ -8,6 +8,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        print 'fake line'
     #     # # Adding model 'Stocks'
     #     # db.create_table(u'reminderapp_stocks', (
     #     #     (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
