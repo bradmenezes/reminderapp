894013ed84c36a49eaeb79f8e5a40dde5af07836
bradmenezes <bradmenezes10@gmail.com>
2014-05-08 23:00:54 -0700

    added paused_at to scheduler model

--- reminderapp/admin.py
+++ reminderapp/admin.py
@@ -15,6 +15,6 @@ admin.site.register(KeyUserData, KeyUserDataAdmin)
 admin.site.register(Stocks, StocksAdmin)
 
 class ScheduleAdmin(admin.ModelAdmin):
-	list_display = ('user','type', 'message', 'created_on', 'modified_date', 'frequency', 'day_of_week', 'start_date', 'hour', 'minute')
+	list_display = ('user','type', 'frequency', 'message', 'start_date', 'hour', 'minute', 'paused_at', 'created_on', 'modified_date', 'day_of_week', )
 
 admin.site.register(Schedule, ScheduleAdmin)
