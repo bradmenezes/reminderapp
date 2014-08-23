48091d31519c97da54a4c4b7ed6f186f1964dcd7
= <bradmenezes10@gmail.com>
2014-08-23 15:37:59 -0700

    removed more django toolbar from settings

--- reminderapp/base_settings.py
+++ reminderapp/base_settings.py
@@ -78,7 +78,6 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
